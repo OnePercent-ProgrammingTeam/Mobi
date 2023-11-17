0f8944010b6e6c58c4d93616f49a7be5bc67c95a
Cristian Scobioala - Nicoglu <t8220137@aueb.gr>
2023-11-17 16:03:26 +0200

    Add javadoc comments to ContainerVisualization.java

--- src/main/java/gr/aueb/dmst/onepercent/programming/ContainerVisualization.java
+++ src/main/java/gr/aueb/dmst/onepercent/programming/ContainerVisualization.java
@@ -23,38 +23,36 @@ import java.util.TimerTask;
 
 public class ContainerVisualization extends JFrame {
     
+    /** Represents a sequence of zero or more data items in the form (x, y) */
     private XYSeries usageSeries;
 
+    /** Constructor 
+     * @param title the title of the window
+     * @variable dataset a collection of XYSeries objects that can be used as a dataset
+    */
     public ContainerVisualization(String title) {
         super(title);
-
-        // Create series/ axis for the CPU usage
         usageSeries = new XYSeries("CPU Usage");
-
         XYSeriesCollection dataset = new XYSeriesCollection(usageSeries);
-
-        // Create a chart
+        
         JFreeChart chart = ChartFactory.createTimeSeriesChart(
                 "Container Stats",
                 "Time",
                 "Usage",
                 dataset
-        );
-
-        // Customize chart setting backround color 
+        ); 
         chart.setBackgroundPaint(Color.white);
-
         XYPlot plot = (XYPlot) chart.getPlot(); // Get the plot from the chart, casting it to XYPlot because it may be a subclass
         DateAxis dateAxis = (DateAxis) plot.getDomainAxis(); // Get the domain axis from the plot, casting it to DateAxis because it may be a subclass
         dateAxis.setTickUnit(new DateTickUnit(DateTickUnitType.SECOND, 1)); // Set the tick unit to be 1 second (it is a real time chart, so we want to update every second)
 
-        // Create Panel, panel is 
+        // The GUI component to make the chart visible to the end user
         ChartPanel chartPanel = new ChartPanel(chart);
         chartPanel.setPreferredSize(new Dimension(560, 370));
         setContentPane(chartPanel);
     }
 
-    // Update the stats with the new data point
+    /** Update the stats with the new data point*/
     private void updateStats(long timestamp, double usage) {
         // Add new data point to the series
         long timestampInMilliseconds = new Date(timestamp).getTime(); // Convert the timestamp to milliseconds, type long
@@ -63,17 +61,17 @@ public class ContainerVisualization extends JFrame {
           the parameters it gets are the x and y values of the data point (should be double)*/
     }
 
+    /** Start updating the stats in order to plot real time data-the consume of cpu resources every second */
     public void startUpdatingStats(CloseableHttpResponse response, ContainerVisualization ex) throws UnsupportedOperationException, IOException {
         // Simulate real-time data update every second
         BufferedReader reader = new BufferedReader(new InputStreamReader(response.getEntity().getContent()));
     
         Timer timer = new Timer(true); // Create a new timer
         timer.scheduleAtFixedRate(new TimerTask() { // Schedule a task to run every second
+             boolean flag = false;
             
-            boolean flag = false;
-    
-            @Override
-            public void run() {
+             @Override
+            public void run() { // The task to run, this will be executed every second
                 try {
                     onRunGraph( flag, reader, ex, timer);
                 } catch (IOException  e) {
@@ -83,6 +81,13 @@ public class ContainerVisualization extends JFrame {
         }, 0, 1000); // Update every second (1000 milliseconds = 1 second) 
     }
     
+    /** Run the graph
+     * @param flag a boolean variable
+     * @param reader a BufferedReader object
+     * @param ex a ContainerVisualization object
+     * @param timer a Timer object
+     * @throws IOException if an error occurs while reading the input
+     */
     public void onRunGraph (boolean flag, BufferedReader reader, ContainerVisualization ex, Timer timer) throws IOException{
         String inputLine = reader.readLine(); // Read a new line from the response
                     if (inputLine != null) {
@@ -90,7 +95,7 @@ public class ContainerVisualization extends JFrame {
                         long currentTimestamp = System.currentTimeMillis(); // Get the current timestamp
                         updateStats(currentTimestamp, d);
                         if (!flag) {
-                            ex.setVisible(true);
+                            ex.setVisible(true); // Make the window visible to the end user, we use flag because we want to make the window visible only once
                             flag = true;
                         }
                     } else {
@@ -101,7 +106,7 @@ public class ContainerVisualization extends JFrame {
                     }
     }
     
-
+    /** Main method */
     public static void main(String[] args) {
         SwingUtilities.invokeLater(() -> { // Create a new thread to run the GUI
             ContainerVisualization example = new ContainerVisualization("Container Stats Plotter"); // Create a new ContainerVisualization object
@@ -112,13 +117,9 @@ public class ContainerVisualization extends JFrame {
            
             try {
                 example.startUpdatingStats(res, example); // Start updating the stats
-                 // Make the window visible to the end user
-                
             } catch (UnsupportedOperationException | IOException e) {
-                // TODO Auto-generated catch block
                 e.printStackTrace();
             } // Start updating the stats
-            //example.setVisible(true); // Make the window visible to the end user 
         });
     }
 }
