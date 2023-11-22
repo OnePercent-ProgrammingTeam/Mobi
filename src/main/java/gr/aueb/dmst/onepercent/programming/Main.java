aa8e29df2cdd4db200c09f54d85ad2c2df829101
zoi limperi <t8220079@aueb.gr>
2023-11-22 02:15:35 +0200

    Change main so as to interact with Threads in Main.java

--- src/main/java/gr/aueb/dmst/onepercent/programming/Main.java
+++ src/main/java/gr/aueb/dmst/onepercent/programming/Main.java
@@ -1,42 +1,35 @@
 package gr.aueb.dmst.onepercent.programming;
+
 import com.github.dockerjava.api.DockerClient;
 import com.github.dockerjava.api.model.Container;
 import com.github.dockerjava.core.DefaultDockerClientConfig;
-import com.github.dockerjava.core.DockerClientBuilder; 
+import com.github.dockerjava.core.DockerClientBuilder;
+
 import java.util.List;
 
-
 public class Main {
-    
-    public static void main(String[] args) throws InterruptedException {
-        
-        /* Used the instructions below instead of the usage of the deprecated method:
-         * DefaultDockerClientConfig.Builder builder = DefaultDockerClientConfig.createDefaultConfigBuilder();
-         *  DockerClient dockerClient = DockerClientBuilder.getInstance(builder).build();
-         */
 
+    public static void main(String[] args) throws InterruptedException {
+        // Initialize Docker client
         DefaultDockerClientConfig config = DefaultDockerClientConfig
-        .createDefaultConfigBuilder()
-        .withDockerHost("tcp://localhost:2375")
-        .build();
+                .createDefaultConfigBuilder()
+                .withDockerHost("tcp://localhost:2375")
+                .build();
         DockerClient dockerClient = DockerClientBuilder.getInstance(config).build();
         dockerClient.versionCmd().exec();
-        
-         
-        
-        List<Container> containers;
-        System.out.println("-----------------------ALL CONTAINER INSTANCES-----------------------");
-        containers= dockerClient.listContainersCmd().withShowAll(true).exec();
-        containers.forEach(c-> System.out.println("ID: " + c.getId() + "\nState: " + c.getState() + "\nStatus: " + c.getStatus() + "\nNetwork: "  + c.getNetworkSettings() + "\nCreated: " + c.getCreated() + "\n\n"));
-        String id = containers.get(0).getId();
-        dockerClient.stopContainerCmd(id).exec();
-        Thread.sleep(1000);
-        System.out.println("-----------------------ACTIVE CONTAINER INSTANCES-----------------------");
-        containers = dockerClient.listContainersCmd().withShowAll(false).exec();
-        containers.forEach(c-> System.out.println(c.getId() + " " + c.getState()));
-        dockerClient.startContainerCmd(id).exec();
-        System.out.println("-----------------------------------");
-        containers = dockerClient.listContainersCmd().withShowAll(false).exec();
-        containers.forEach(c-> System.out.println(c.getId() + " " + c.getState()));
+
+        // Create and start the MonitorThread
+        MonitorThread monitorThread = new MonitorThread(dockerClient);
+      
+        Thread thread= new Thread(monitorThread);
+        thread.start();
+        // Create and start the ExecutorThread
+        //ExecutorThread executorThread = new ExecutorThread(dockerClient);
+       // executorThread.start();
+        //monitorThread.join();
+        //executorThread.join(); //use the join method to wait for the monitorThread and executorThread to finish. This ensures that the main method doesn't exit until both threads have completed their execution.
     }
 }
+
+
+
