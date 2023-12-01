6f07564f84f999becc5fa23b424440dfd3c49196
Cristian Scobioala - Nicoglu <t8220137@aueb.gr>
2023-12-01 02:53:32 +0200

    Totally modify MonitorThread.java so as to contain the appropriate functionalities
    Monitor Thread has three core functionalities: 1. get information abput a container  2. plot a graph with the cpu usage of a running container and 3. search images (brings top three results)

--- src/main/java/gr/aueb/dmst/onepercent/programming/MonitorThread.java
+++ src/main/java/gr/aueb/dmst/onepercent/programming/MonitorThread.java
@@ -7,50 +7,26 @@ import com.github.dockerjava.api.model.Container;
 import java.util.List;
 
 public class MonitorThread extends Thread implements Runnable {
+    private int userInput;
 
-    private DockerClient dockerClient; // declare a field dockerClient of type DockerClient that allows interaction with the Docker daemon.
-    
-
-    public MonitorThread(DockerClient dockerClient) {
-        this.dockerClient = dockerClient;
-    } // the constructor of the MonitorThread class, which takes a DockerClient as a parameter and initializes the dockerClient field
+    protected void setUserInput(int userInput) {
+        this.userInput = userInput;
+    }
 
     @Override
-    public void run() { // run method, which will be executed when the thread starts
-        while (true) { //start a loop The thread will keep running as long as the program is running
-            // Display information about containers
-            //displayContainerInfo(); //call the displayContainerInfo method to show information about Docker containers
-            InteractionwithUser obj = new InteractionwithUser();
-            obj.printMenu();
-            
-
-            try { //This block puts the thread to sleep for a specified interval (in this case, 5000 milliseconds or 5 seconds) before looping again. The InterruptedException is caught in case of interruption during sleep.
-                // Sleep for a specific interval before displaying information again
-                Thread.sleep(5000); // Adjust the interval as needed (in milliseconds)
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
+    public void run() { 
+        var containerMonitorHttp = new ContainerMonitorHttp();
+        if (this.userInput == 3) {
+            containerMonitorHttp.getContainerInformation();
+        } else if (this.userInput == 4) {
+            ContainerVisualization.main(new String[] {"Plot","Diagram"});    
+        } else if (this.userInput ==5 ) {
+            containerMonitorHttp.searchImages(); 
         }
-    }
-
-    private void displayContainerInfo() { //declare the displayContainerInfo method, which is responsible for retrieving and displaying information about Docker containers
-        System.out.println("-----------------------CONTAINER INFORMATION-----------------------");
-
-        // Get the list of all containers
-        List<Container> containers = dockerClient.listContainersCmd().withShowAll(true).exec(); //use the dockerClient to get a list of all containers, including stopped ones
         
-       
-
-        // Display information for each container
-        for (Container container : containers) { //start a loop that iterates through each container in the list.
-            System.out.println("ID: " + container.getId());
-            System.out.println("State: " + container.getState());
-            System.out.println("Status: " + container.getStatus());
-            System.out.println("Network: " + container.getNetworkSettings());
-            System.out.println("Created: " + container.getCreated());
-            System.out.println("-----------------------------------"); 
-        }
+    
     }
-} //print various information about each container, such as ID, state, status, network settings, and creation time
 
+}
+ 
 
