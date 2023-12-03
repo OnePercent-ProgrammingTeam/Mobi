5dc8ff30d7a9324ae8fca2b601eaaa9e9bc99464
Cristian Scobioala - Nicoglu <t8220137@aueb.gr>
2023-12-03 13:30:21 +0200

    Optimize the code quality of ContainerHttpRequest.java
    This is a stage before encorporating checkstyle in our project for the best possible code quality

--- src/main/java/gr/aueb/dmst/onepercent/programming/ContainerHttpRequest.java
+++ src/main/java/gr/aueb/dmst/onepercent/programming/ContainerHttpRequest.java
@@ -1,7 +1,5 @@
 package gr.aueb.dmst.onepercent.programming;
 
-
-
 import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.client.methods.HttpPost;
@@ -9,14 +7,14 @@ import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
 
 public abstract class ContainerHttpRequest {
-    /** Docker host address*/ 
+    /** Docker host address */
     protected static final String DOCKER_HOST = "http://localhost:2375";
 
     /** Create an http client */
-    protected static final CloseableHttpClient HTTP_CLIENT = HttpClients.createDefault(); 
+    protected static final CloseableHttpClient HTTP_CLIENT = HttpClients.createDefault();
 
     /** Http Post request (Post is "do to something e.g start container") */
-    protected static HttpPost postRequest; 
+    protected static HttpPost postRequest;
     protected static String containerId;
 
     /** Http Get request (Get is "to get something e.g info about containers") */
@@ -27,9 +25,10 @@ public abstract class ContainerHttpRequest {
     protected static StringBuffer response1;
     /** Last CPU Usage is the last metric for a running container */
     protected static double lastCPUUsage;
+
     public abstract void executeHttpRequest(String message);
 
-    public CloseableHttpResponse getHttpResponse(String message){
+    public CloseableHttpResponse getHttpResponse(String message) {
         return response;
     }
 
