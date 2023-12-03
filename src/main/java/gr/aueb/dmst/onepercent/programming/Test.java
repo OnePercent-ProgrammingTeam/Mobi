257940120c497e86d5cfc01f3e0a080498b9b80c
Cristian Scobioala - Nicoglu <t8220137@aueb.gr>
2023-12-03 13:35:37 +0200

    Optimize the code quality of Test.java
    This is a stage before encorporating checkstyle in our project for the best possible code quality

--- src/main/java/gr/aueb/dmst/onepercent/programming/Test.java
+++ src/main/java/gr/aueb/dmst/onepercent/programming/Test.java
@@ -1,31 +1,32 @@
 package gr.aueb.dmst.onepercent.programming;
+
 import java.io.IOException;
 import java.util.Scanner;
+
 public class Test {
 
-    //handle user input and make sure it is valid
-    public static String handleInput(String outputMessage){
+    // handle user input and make sure it is valid
+    public static String handleInput(String outputMessage) {
         Scanner sc = new Scanner(System.in);
         System.out.println(outputMessage);
         String input = sc.next();
-        sc.nextLine();//clear buffer
+        sc.nextLine();// clear buffer
         return input;
-    } 
+    }
 
-    public static void main (String[] args) {
-        
-        //ContainerManager cm = new ContainerManager();
+    public static void main(String[] args) {
+
+        // ContainerManager cm = new ContainerManager();
         ContainerManager.createDockerClient();
-        //cm.startContainer();
-        //cm.stopContainer(); 
-         //ContainerMonitorHttp obj = new ContainerMonitorHttp();
-        //obj.getContainerStats();
+        // cm.startContainer();
+        // cm.stopContainer();
+        // ContainerMonitorHttp obj = new ContainerMonitorHttp();
+        // obj.getContainerStats();
         // CSV csv = new CSV();
-        //csv.writeContainerInfoToCsv();
-               
+        // csv.writeContainerInfoToCsv();
+
         MenuThread menuThread = new MenuThread();
         Thread thread = new Thread(menuThread);
         thread.start();
     }
 }
-
