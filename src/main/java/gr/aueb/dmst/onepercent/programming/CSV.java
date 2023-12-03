39a380fedc1780d63b4e8ca3606f47cae0b33800
Cristian Scobioala - Nicoglu <t8220137@aueb.gr>
2023-12-03 13:29:25 +0200

    Change the the code quality of CSV.java
    This is a stage before encorporating checkstyle in our project for the best possible code quality

--- src/main/java/gr/aueb/dmst/onepercent/programming/CSV.java
+++ src/main/java/gr/aueb/dmst/onepercent/programming/CSV.java
@@ -1,4 +1,5 @@
 package gr.aueb.dmst.onepercent.programming;
+
 import com.opencsv.CSVWriter;
 
 import java.io.File;
@@ -12,58 +13,59 @@ import java.util.TimerTask;
 import java.util.Scanner;
 
 public class CSV {
-    
-    /**Create a csv file with the name of the container, each "column" is:
-     * Container Name, Container Id, IP Address, Mac Address, CPU Usage
-    */
 
-    
+    /**
+     * Create a csv file with the name of the container, each "column" is:
+     * Container Name, Container Id, IP Address, Mac Address, CPU Usage
+     */
+
     ContainerMonitorHttp containerMonitorHttp = new ContainerMonitorHttp();
     private String folderPath;
-    private static final String[] HEADER = {"Container Name", "Container Id", "IP Address", "Mac Address", "CPU Usage", "Date Time"};
+    private static final String[] HEADER = { "Container Name", "Container Id", "IP Address", "Mac Address", "CPU Usage",
+            "Date Time" };
 
-    public void createDataFolder(String userPath) {  //Take for input the folder path given by the user
-        
-        folderPath = Paths.get(userPath, "Docker Data").toString(); 
+    public void createDataFolder(String userPath) { // Take for input the folder path given by the user
+
+        folderPath = Paths.get(userPath, "Docker Data").toString();
         File folder = new File(folderPath);// Create a File object representing the directory
-        folder.mkdirs(); // Create the directory and parent directories if they don't exist  
+        folder.mkdirs(); // Create the directory and parent directories if they don't exist
     }
 
-    public void createFile (String filePath) {
+    public void createFile(String filePath) {
         File file = new File(filePath);
-        try (CSVWriter writer = new CSVWriter(new FileWriter(filePath, true))){
-            
-            /*If file does not exist OR it has no content included in it,
+        try (CSVWriter writer = new CSVWriter(new FileWriter(filePath, true))) {
+
+            /*
+             * If file does not exist OR it has no content included in it,
              * then print the name of the columns
              */
-            if (!(file.exists() && (file.length() > 0))) { 
+            if (!(file.exists() && (file.length() > 0))) {
                 writer.writeNext(HEADER);
             }
         } catch (IOException e) {
             e.printStackTrace();
         }
 
-        
     }
-    
+
     public void getRealTimeData() {
         containerMonitorHttp.getContainerStats();
         String[] info = containerMonitorHttp.prepareStorageData();
-        //String filename = info[0] + "Data";
-        //String filePath = filename +".csv"; //create a file in the specified path
+        // String filename = info[0] + "Data";
+        // String filePath = filename +".csv"; //create a file in the specified path
         System.out.println("OK");
-        String filePath = Paths.get(folderPath, info[0] +"Data.csv").toString();
+        String filePath = Paths.get(folderPath, info[0] + "Data.csv").toString();
         createFile(filePath);
         Timer timer = new Timer(false); // Create a new timer
         timer.scheduleAtFixedRate(new TimerTask() { // Schedule a task to run every second
-            
+
             @Override
             public void run() { // The task to run, this will be executed every second
-                    
-                try (CSVWriter writer = new CSVWriter(new FileWriter(filePath,true))){
-                    
+
+                try (CSVWriter writer = new CSVWriter(new FileWriter(filePath, true))) {
+
                     String[] info = containerMonitorHttp.prepareStorageData();
-                    writer.writeNext(info);    
+                    writer.writeNext(info);
                 } catch (IOException e) {
                     e.printStackTrace();
                 }
@@ -72,16 +74,16 @@ public class CSV {
         }, 0, 1000);
 
     }
-    public static void main(String[] args){
-        
+
+    public static void main(String[] args) {
+
         CSV csv = new CSV();
         System.out.println("Give your path: ");
-        
+
         Scanner sc = new Scanner(System.in);
         final String path = sc.nextLine();
-        csv.createDataFolder(path); 
-        
+        csv.createDataFolder(path);
+
         csv.getRealTimeData();
     }
 }
-
