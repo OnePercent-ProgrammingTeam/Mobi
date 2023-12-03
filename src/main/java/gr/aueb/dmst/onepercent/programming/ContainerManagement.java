c3ddc2c447d43154b262ee0d664a89a02aff6103
Cristian Scobioala - Nicoglu <t8220137@aueb.gr>
2023-12-03 13:30:55 +0200

    Optimize the code quality of ContainerManagement.java
    This is a stage before encorporating checkstyle in our project for the best possible code quality

--- src/main/java/gr/aueb/dmst/onepercent/programming/ContainerManagement.java
+++ src/main/java/gr/aueb/dmst/onepercent/programming/ContainerManagement.java
@@ -1,31 +1,31 @@
 package gr.aueb.dmst.onepercent.programming;
+
 import com.github.dockerjava.api.DockerClient;
 import com.github.dockerjava.core.DefaultDockerClientConfig;
 import com.github.dockerjava.core.DockerClientBuilder;
-import com.github.dockerjava.api.model.Container;
 import java.util.Scanner;
-import java.util.List;
+
 
 public class ContainerManagement {
-    
+
     /** Static variable, used in many classes, representing the docker client */
     protected static DockerClient dc;
-    /** Global variable scanner, used in the methods below */    
-    Scanner sc = new Scanner(System.in); //scanner for user input
+    /** Global variable scanner, used in the methods below */
+    Scanner sc = new Scanner(System.in); // scanner for user input
 
     protected String input;
 
     /** Create a docker client */
     public static void createDockerClient() {
         DefaultDockerClientConfig config = DefaultDockerClientConfig
-        .createDefaultConfigBuilder()
-        .withDockerHost("tcp://localhost:2375") //daemon host
-        .build();
+                .createDefaultConfigBuilder()
+                .withDockerHost("tcp://localhost:2375") // daemon host
+                .build();
         dc = DockerClientBuilder.getInstance(config).build();
         dc.versionCmd().exec();
     }
 
-    public String handleInput(){
+    public String handleInput() {
         return this.input;
     }
 }
