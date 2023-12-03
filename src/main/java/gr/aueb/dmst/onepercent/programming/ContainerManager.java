6767b58c15f8a44e700413b52e272e962b246273
Cristian Scobioala - Nicoglu <t8220137@aueb.gr>
2023-12-03 13:31:28 +0200

    Optimize the code quality of ContainerManager.java
    This is a stage before encorporating checkstyle in our project for the best possible code quality

--- src/main/java/gr/aueb/dmst/onepercent/programming/ContainerManager.java
+++ src/main/java/gr/aueb/dmst/onepercent/programming/ContainerManager.java
@@ -1,4 +1,5 @@
 package gr.aueb.dmst.onepercent.programming;
+
 import com.github.dockerjava.api.DockerClient;
 import com.github.dockerjava.core.DefaultDockerClientConfig;
 import com.github.dockerjava.core.DockerClientBuilder;
@@ -8,25 +9,25 @@ import java.util.List;
 
 public class ContainerManager extends ContainerManagement {
 
-  
-    
     /** Handle user input and make sure it is valid */
     @Override
-    public String handleInput(){
+    public String handleInput() {
         System.out.println("Please enter the id of the container you want to start/stop");
         String input = sc.next();
         if (!checkAllContainerStatus(input)) {
             System.out.println("Container does not exist");
             return null;
         }
-        sc.nextLine(); //clear buffer
+        sc.nextLine(); // clear buffer
         return input;
-    } 
-    
+    }
+
     /** Start container and check if it is already started */
     public void startContainer() {
         String lastIdInput = handleInput();
-        if (lastIdInput == null){return;} 
+        if (lastIdInput == null) {
+            return;
+        }
         if (checkActiveContainerStatus(lastIdInput)) {
             System.out.println("Container already started");
             return;
@@ -38,8 +39,10 @@ public class ContainerManager extends ContainerManagement {
     /** Stop container, practicly kill it and check if it is already stopped */
     public void stopContainer() {
         String lastIdInput = handleInput();
-        if (lastIdInput == null){return;} 
-        if(!checkActiveContainerStatus(lastIdInput)) {
+        if (lastIdInput == null) {
+            return;
+        }
+        if (!checkActiveContainerStatus(lastIdInput)) {
             System.out.println("Container already stopped");
             return;
         }
@@ -47,30 +50,28 @@ public class ContainerManager extends ContainerManagement {
         System.out.println("Container stopped");
     }
 
-
     /** Check if container is active */
     public boolean checkActiveContainerStatus(String idInput) {
         List<Container> containers;
-        containers= dc.listContainersCmd().withShowAll(false).exec();
+        containers = dc.listContainersCmd().withShowAll(false).exec();
         for (Container c : containers) {
             if (c.getId().equals(idInput)) {
-                return true; //container is active
+                return true; // container is active
             }
         }
-        return false; //container is not active
+        return false; // container is not active
     }
 
     /** Check if container exists */
     public boolean checkAllContainerStatus(String idInput) {
         List<Container> containers;
-        containers= dc.listContainersCmd().withShowAll(true).exec();
+        containers = dc.listContainersCmd().withShowAll(true).exec();
         for (Container c : containers) {
             if (c.getId().equals(idInput)) {
-                return true; //container exists
+                return true; // container exists
             }
         }
-        return false;//container does not exist
+        return false;// container does not exist
     }
 
 }
-        
