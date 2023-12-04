3ba38448d1dac48e894c150c47618b19d52df81a
Cristian Scobioala - Nicoglu <t8220137@aueb.gr>
2023-12-04 21:53:57 +0200

    Change the call of a method previously named getContainerInformation() to inspectContainer()

--- src/main/java/gr/aueb/dmst/onepercent/programming/MonitorThread.java
+++ src/main/java/gr/aueb/dmst/onepercent/programming/MonitorThread.java
@@ -5,7 +5,7 @@ public class MonitorThread extends SuperThread {
     public void run() { 
         var containerMonitorHttp = new ContainerMonitorHttp();
         if (this.userInput == 3) {
-            containerMonitorHttp.getContainerInformation();
+            containerMonitorHttp.inspectContainer();
         } else if (this.userInput == 4) {
             ContainerVisualization.executeDiagram();    
         } else if (this.userInput == 5 ) {
