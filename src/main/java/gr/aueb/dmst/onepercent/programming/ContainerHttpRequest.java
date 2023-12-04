9e564c6e611af0b9d7cfb2f8a941e87cd7cc2fea
Dionisia <t8220066@aueb.gr>
2023-12-04 18:58:45 +0200

    Change the access modifiers of fields
    We have altered the access modifiers of many fields from protected to public so as to be able to make tests. After the tests, we have changed them to protected again

--- src/main/java/gr/aueb/dmst/onepercent/programming/ContainerHttpRequest.java
+++ src/main/java/gr/aueb/dmst/onepercent/programming/ContainerHttpRequest.java
@@ -6,30 +6,31 @@ import org.apache.http.client.methods.HttpPost;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
 
-public abstract class ContainerHttpRequest {
-    /** Docker host address */
-    protected static final String DOCKER_HOST = "http://localhost:2375";
+public abstract class ContainerHttpRequest implements HttpInterface {
+    /** Docker host address*/ 
+    protected static final String DOCKER_HOST = "http://localhost:2375"; //used in junit test
 
     /** Create an http client */
-    protected static final CloseableHttpClient HTTP_CLIENT = HttpClients.createDefault();
+    protected static final CloseableHttpClient HTTP_CLIENT = HttpClients.createDefault(); 
 
-    /** Http Post request (Post is "do to something e.g start container") */
-    protected static HttpPost postRequest;
-    protected static String containerId;
+    /** Http Post request (Post is "to request to do something e.g start container") */
+    protected static HttpPost postRequest; 
+    protected static String containerId; //used in junit test
 
-    /** Http Get request (Get is "to get something e.g info about containers") */
+    /** Http Get request (Get is "to request to get something e.g info about containers") */
     protected static HttpGet getRequest;
     protected static String imageName;
 
     protected CloseableHttpResponse response;
-    protected static StringBuffer response1;
+    protected static StringBuffer response1; //used in junit test
+
     /** Last CPU Usage is the last metric for a running container */
     protected static double lastCPUUsage;
 
-    public abstract void executeHttpRequest(String message);
+    //This will be implemented by children of the class 
+    public void executeHttpRequest(String message){};
 
-    public CloseableHttpResponse getHttpResponse(String message) {
+    public CloseableHttpResponse getHttpResponse(String message){
         return response;
     }
-
 }
