13d2c6ada5fbcce6721637f260823534ec1f63ea
Cristian Scobioala - Nicoglu <t8220137@aueb.gr>
2023-12-04 20:17:52 +0200

    Add javadoc comments in ContainerHttpRequest.java

--- src/main/java/gr/aueb/dmst/onepercent/programming/ContainerHttpRequest.java
+++ src/main/java/gr/aueb/dmst/onepercent/programming/ContainerHttpRequest.java
@@ -7,30 +7,54 @@ import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
 
 public abstract class ContainerHttpRequest implements HttpInterface {
-    /** Docker host address*/ 
-    protected static final String DOCKER_HOST = "http://localhost:2375"; //used in junit test
+    /** Docker host address */
+    protected static final String DOCKER_HOST = "http://localhost:2375"; // used in junit test
 
     /** Create an http client */
-    protected static final CloseableHttpClient HTTP_CLIENT = HttpClients.createDefault(); 
+    protected static final CloseableHttpClient HTTP_CLIENT = HttpClients.createDefault();
 
-    /** Http Post request (Post is "to request to do something e.g start container") */
-    protected static HttpPost postRequest; 
-    protected static String containerId; //used in junit test
+    /**
+     * Http Post request (Post is "to request to do something e.g start container")
+     */
+    protected static HttpPost postRequest;
+    /**
+     * Container id of the container that is going to be started, stopped or
+     * inspected
+     */
+    protected static String containerId; // used in junit test
 
-    /** Http Get request (Get is "to request to get something e.g info about containers") */
+    /**
+     * Http Get request (Get is "to request to get something e.g info about
+     * containers")
+     */
     protected static HttpGet getRequest;
+    /** Image name of the image that is going to be pulled or searched */
     protected static String imageName;
-
+    /** Http response return by the executed http request */
     protected CloseableHttpResponse response;
-    protected static StringBuffer response1; //used in junit test
+    /** Http response, read using StringBuffer */
+    protected static StringBuffer response1; // used in junit test
 
     /** Last CPU Usage is the last metric for a running container */
     protected static double lastCPUUsage;
 
-    //This will be implemented by children of the class 
-    public void executeHttpRequest(String message){};
+    /**
+     * Execute the http request
+     * 
+     * @param message the message that indicates the action that is going to be
+     *                executed
+     */
+    public void executeHttpRequest(String message) {
+    };
 
-    public CloseableHttpResponse getHttpResponse(String message){
+    /**
+     * Get the response of the http request
+     * 
+     * @param message the message that indicates the action that is going to be
+     *                executed
+     *                it is the query for the http request
+     */
+    public CloseableHttpResponse getHttpResponse(String message) {
         return response;
     }
 }
