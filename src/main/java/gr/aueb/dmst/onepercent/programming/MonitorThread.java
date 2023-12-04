c5793ea26a2fbd8fe88c4dcccd423682ac6aefef
Dionisia <t8220066@aueb.gr>
2023-12-04 19:05:36 +0200

    Express inheritance relatioship between MonitorThread.java and SuperThread.java

--- src/main/java/gr/aueb/dmst/onepercent/programming/MonitorThread.java
+++ src/main/java/gr/aueb/dmst/onepercent/programming/MonitorThread.java
@@ -1,28 +1,18 @@
 package gr.aueb.dmst.onepercent.programming;
 
-import com.github.dockerjava.api.DockerClient;
-import com.github.dockerjava.api.model.Container;
-
-import java.util.List;
-
-public class MonitorThread extends Thread implements Runnable {
-    private int userInput;
-
-    protected void setUserInput(int userInput) {
-        this.userInput = userInput;
-    }
-
+public class MonitorThread extends SuperThread {
     @Override
-    public void run() {
+    public void run() { 
         var containerMonitorHttp = new ContainerMonitorHttp();
         if (this.userInput == 3) {
             containerMonitorHttp.getContainerInformation();
         } else if (this.userInput == 4) {
-            ContainerVisualization.main(new String[] { "Plot", "Diagram" });
-        } else if (this.userInput == 5) {
-            containerMonitorHttp.searchImages();
+            ContainerVisualization.executeDiagram();    
+        } else if (this.userInput == 5 ) {
+            containerMonitorHttp.searchImages(); 
+        } else if (this.userInput == 6) {
+            CSV csv = new CSV();
+            csv.startSavingData();
         }
-
     }
-
 }
