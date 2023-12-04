f6b607fdda7a651f8a9c51b916c56caef6309811
Dionisia <t8220066@aueb.gr>
2023-12-04 18:41:18 +0200

    Fix the code quality of DockerClusterMonitoring.java

--- src/main/java/gr/aueb/dmst/onepercent/programming/DockerClusterMonitoring.java
+++ src/main/java/gr/aueb/dmst/onepercent/programming/DockerClusterMonitoring.java
@@ -3,17 +3,16 @@ package gr.aueb.dmst.onepercent.programming;
 import com.github.dockerjava.api.DockerClient;
 import com.github.dockerjava.core.DefaultDockerClientConfig;
 import com.github.dockerjava.core.DockerClientBuilder;
-import com.github.dockerjava.api.model.SwarmNode;
+import com.github.dockerjava.api.model.SwarmNode; 
 
 public class DockerClusterMonitoring {
     public static void main(String[] args) {
         // Create a Docker client
-        DefaultDockerClientConfig config = DefaultDockerClientConfig.createDefaultConfigBuilder()
-                .withDockerHost("tcp://localhost:2375").build();
+        DefaultDockerClientConfig config = DefaultDockerClientConfig.createDefaultConfigBuilder().withDockerHost("tcp://localhost:2375").build();
         DockerClient dockerClient = DockerClientBuilder.getInstance(config).build();
         dockerClient.versionCmd().exec();
-        // after initializing a docker swarm on your machine, use a loop to print infos
-        // about the nodes of the cluster
+
+        // after initializing a docker swarm on your machine, use a loop to print infos about the nodes of the cluster
         for (SwarmNode node : dockerClient.listSwarmNodesCmd().exec()) {
             System.out.println("Node ID: " + node.getId());
             System.out.println("Node State: " + node.getStatus().getState());
@@ -21,7 +20,7 @@ public class DockerClusterMonitoring {
             System.out.println("Node Version" + node.getVersion());
             System.out.println("Node Created at: " + node.getCreatedAt());
             System.out.println("Node Updated at: " + node.getUpdatedAt());
-            System.out.println("Node Description: " + node.getDescription());
+            System.out.println("Node Description: " + node.getDescription()); 
         }
     }
 }
