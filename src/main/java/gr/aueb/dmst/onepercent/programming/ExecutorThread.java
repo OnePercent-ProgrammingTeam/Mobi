3340ac6bfda0400d1e5eef03472c08587a61762f
Dionisia <t8220066@aueb.gr>
2023-12-04 19:05:02 +0200

    Express inheritance relatioship between ExecutorThread.java and SuperThread.java

--- src/main/java/gr/aueb/dmst/onepercent/programming/ExecutorThread.java
+++ src/main/java/gr/aueb/dmst/onepercent/programming/ExecutorThread.java
@@ -1,21 +1,13 @@
 package gr.aueb.dmst.onepercent.programming;
 
-public class ExecutorThread implements Runnable {
-    private int userInput;
-
-    protected void setUserInput(int userInput) {
-        this.userInput = userInput;
-    }
-
+public class ExecutorThread extends SuperThread {
     @Override
-    public void run() {
+    public void run() { 
         var containerManagerHttp = new ContainerManagerHttp();
         if (this.userInput == 1) {
             containerManagerHttp.startContainer();
         } else if (this.userInput == 2) {
             containerManagerHttp.stopContainer();
-        }
-
-    }
-
+        }   
+    }  
 }
