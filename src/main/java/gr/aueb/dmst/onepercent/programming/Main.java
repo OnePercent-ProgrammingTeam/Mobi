e1d7611aa15812a9b30668ae74f7e676c8c505a1
Dionisia <t8220066@aueb.gr>
2023-12-04 19:06:43 +0200

    Change Main.java so as to run the entire program

--- src/main/java/gr/aueb/dmst/onepercent/programming/Main.java
+++ src/main/java/gr/aueb/dmst/onepercent/programming/Main.java
@@ -1,34 +1,23 @@
 package gr.aueb.dmst.onepercent.programming;
 
-import com.github.dockerjava.api.DockerClient;
-import com.github.dockerjava.api.model.Container;
-import com.github.dockerjava.core.DefaultDockerClientConfig;
-import com.github.dockerjava.core.DockerClientBuilder;
-
-import java.util.List;
+import java.util.Scanner;
 
 public class Main {
+    static final Scanner SC = new Scanner(System.in);
 
-    public static void main(String[] args) throws InterruptedException {
-        // Initialize Docker client
-        DefaultDockerClientConfig config = DefaultDockerClientConfig
-                .createDefaultConfigBuilder()
-                .withDockerHost("tcp://localhost:2375")
-                .build();
-        DockerClient dockerClient = DockerClientBuilder.getInstance(config).build();
-        dockerClient.versionCmd().exec();
+    //handle user input and make sure it is valid
+    public static String handleInput(String outputMessage){
+        
+        System.out.println(outputMessage);
+        String input = SC.next();
+        System.out.println();
+        SC.nextLine();//clear buffer
+        return input;
+    } 
 
-        // Create and start the MonitorThread
-        MonitorThread monitorThread = new MonitorThread();
-
-        Thread thread = new Thread(monitorThread);
+    public static void main(String[] args) {
+        MenuThread menuThread = new MenuThread();
+        Thread thread = new Thread(menuThread);
         thread.start();
-        // Create and start the ExecutorThread
-        // ExecutorThread executorThread = new ExecutorThread(dockerClient);
-        // executorThread.start();
-        // monitorThread.join();
-        // executorThread.join(); //use the join method to wait for the monitorThread
-        // and executorThread to finish. This ensures that the main method doesn't exit
-        // until both threads have completed their execution.
     }
 }
