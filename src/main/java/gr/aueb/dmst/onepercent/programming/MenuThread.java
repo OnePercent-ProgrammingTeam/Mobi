6ca6d4f61b97240eb014dde335002f76c609347f
Cristian Scobioala - Nicoglu <t8220137@aueb.gr>
2023-12-05 23:19:13 +0200

    Add extra options for the user in MenuThread.java

--- src/main/java/gr/aueb/dmst/onepercent/programming/MenuThread.java
+++ src/main/java/gr/aueb/dmst/onepercent/programming/MenuThread.java
@@ -3,6 +3,7 @@ package gr.aueb.dmst.onepercent.programming;
 import java.util.InputMismatchException;
 import java.util.Scanner;
 
+/** Class: MenuThread is a thread that prints the menu and handles the user's input */
 public class MenuThread extends Thread {
    
     private final Scanner INPUT = new Scanner(System.in);
@@ -15,20 +16,35 @@ public class MenuThread extends Thread {
         printMenu(); 
     }
 
+    /** Method: printMenu() prints the main menu of the program indicating
+     * the available options to the user
+    */
     public void printMenu(){
+        System.out.println("-LIST OF CONTAINERS-");
+        System.out.println("------------------------------------------------------------");
+        SuperAPI.createDockerClient();
+        MonitorAPI containerMonitor = new MonitorAPI();
+        containerMonitor.initializeContainerModels();
+        containerMonitor.getContainerList();
+        System.out.println("\n");
         do{
             System.out.println("-MENU-");
             System.out.println("------------------------------------------------------------");
             System.out.println("1) Start container");
             System.out.println("2) Stop container");
-            System.out.println("3) Get information about a specific container");
-            System.out.println("4) Plot CPU Usage diagram for a running container");
-            System.out.println("5) Search image");
-            System.out.println("6) Store real-time data in .csv file for a running container");
+            System.out.println("3) Search image");
+            System.out.println("4) Pull image");
+            System.out.println("5) Plot CPU Usage diagram for a running container");
+            System.out.println("6) Get information about a specific container");
+            System.out.println("7) Store real-time data in .csv file for a running container");
+            System.out.println("8) Print a list with the locally installed containers");
             System.out.println("-------------------------------------------------------------");
         } while (handleUserInput());
     }
     
+    /** Method: handleUserInput() handles the user's input
+     * @return true if the user wants to run the program again, false otherwise
+     */
     public boolean handleUserInput() {
         System.out.print("Please enter a number: ");
         int answer = 0;
@@ -40,13 +56,13 @@ public class MenuThread extends Thread {
         }
         
         switch (answer) {
-            case 1,2:
+            case 1,2,4:
                 executorThread.setUserInput(answer);
                 thread = new Thread(executorThread);
                 thread.setName("Executor"); // set name to the thread so as to be easier to recognize it. 
                 thread.start();
                 break;
-            case 3,4,5,6:
+            case 3,5,6,7,8:
                 monitorThread.setUserInput(answer);
                 thread = new Thread(monitorThread);
                 thread.setName("Monitor");
