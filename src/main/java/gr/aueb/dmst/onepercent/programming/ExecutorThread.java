8eb4ca61e56336b4731a4c03e706d3fc39b44da6
Cristian Scobioala - Nicoglu <t8220137@aueb.gr>
2023-12-05 23:18:00 +0200

    Add pull image functionality in ExecutoThread.java

--- src/main/java/gr/aueb/dmst/onepercent/programming/ExecutorThread.java
+++ src/main/java/gr/aueb/dmst/onepercent/programming/ExecutorThread.java
@@ -1,13 +1,23 @@
 package gr.aueb.dmst.onepercent.programming;
 
+/**  Class: ExecutorThread is the thread that primarly executes the
+ *  core functinalities described below.
+ *  1. Starts a container 
+ *  2. Stops a container
+ *  The user is asked to type the id of the container he wants to start/stop.
+ *  He is able to choose from a list of containers that are already installed
+ *  in his machine.
+ */
 public class ExecutorThread extends SuperThread {
     @Override
     public void run() { 
-        var containerManagerHttp = new ContainerManagerHttp();
+        var containerManagerHttp = new ManagerHttp();
         if (this.userInput == 1) {
             containerManagerHttp.startContainer();
         } else if (this.userInput == 2) {
             containerManagerHttp.stopContainer();
-        }   
+        } else if (this.userInput == 4) {
+            containerManagerHttp.pullImage();
+        }
     }  
 }
