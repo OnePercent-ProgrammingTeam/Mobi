9f4f1a5e9a3ee97608379c402e3b49af9bbb8338
Cristian Scobioala - Nicoglu <t8220137@aueb.gr>
2023-12-05 23:25:52 +0200

    Rename ContainerHttpRequest.java to SuperAPI.java

---
+++ src/main/java/gr/aueb/dmst/onepercent/programming/SuperAPI.java
@@ -0,0 +1,26 @@
+package gr.aueb.dmst.onepercent.programming;
+//package gr.aueb.dmst.onepercent.programming;
+
+import com.github.dockerjava.api.DockerClient;
+import com.github.dockerjava.core.DefaultDockerClientConfig;
+import com.github.dockerjava.core.DockerClientBuilder;
+
+/** Class: SuperAPI is a superclass that contains a static variable, used in other classes, representing the docker client.
+ * It is used to create a docker client.
+ */
+public class SuperAPI {
+    
+    /** Field: dc is a static variable, used in many classes, representing the docker client */
+    protected static DockerClient dc;
+
+    /** Method: createDockerClient() creates a docker client */
+    public static void createDockerClient() {
+        DefaultDockerClientConfig config = DefaultDockerClientConfig
+        .createDefaultConfigBuilder()
+        .withDockerHost("tcp://localhost:2375") //daemon host
+        .build();
+        dc = DockerClientBuilder.getInstance(config).build();
+        dc.versionCmd().exec();
+    }
+
+}
