b110e92710a6f0e8546f0d9e2f171a315c4cd4e2
Dionisia <t8220066@aueb.gr>
2023-12-30 15:58:30 +0200

    Add the class DataBaseThread.java that creates a thread for database actions

---
+++ src/main/java/gr/aueb/dmst/onepercent/programming/DataBaseThread.java
@@ -0,0 +1,34 @@
+package gr.aueb.dmst.onepercent.programming;
+
+/** Class: DataBaseThread is a class that contains methods that 
+ *  insert data in the tables so as to store the actions of the customer.
+ *  It is a subclass of SuperThread.
+ *  @see SuperThread
+ */
+
+public class DataBaseThread extends SuperThread {
+    @Override
+    public void run() {
+        DataBase database = new DataBase();
+        String datetime;
+        int last_id;
+
+        if (this.userInput == 1 || this.userInput == 2) {
+            datetime = database.getDateTime();
+            last_id = database.insertMetricsToDatabase(datetime);
+            database.insertContainerToDatabase(last_id);
+            database.getAllMetrics(); //helpful
+            database.getAllContainer(); //helpful
+
+
+        } else if (this.userInput == 3) {
+            datetime = database.getDateTime();
+            last_id = database.insertMetricsToDatabase(datetime);
+            database.insertImageToDatabase(last_id);
+            database.getAllMetrics(); //helpful
+            database.getAllImage(); //helpful
+
+        } 
+    }
+    
+}
