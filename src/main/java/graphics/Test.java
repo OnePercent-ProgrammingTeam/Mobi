80bb2971c9c52083f3fd9b3124ec2f397335a5fb
Cristian Scobioala - Nicoglu <t8220137@aueb.gr>
2024-01-01 16:22:02 +0200

    Delete Test.java, test class from graphics/

--- src/main/java/graphics/Test.java
+++
@@ -1,8 +0,0 @@
-package graphics;
-import java.time.LocalTime;
-public class Test {
-    public static void main(String[] args) {
-        System.out.println("wduwudhukwdewjb");
-        LocalTime.now();
-    }
-}
