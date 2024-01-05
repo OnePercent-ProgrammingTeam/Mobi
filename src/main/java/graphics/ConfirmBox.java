1818ffb97e0331550c56121d1fb434f21cb07425
Cristian Scobioala - Nicoglu <t8220137@aueb.gr>
2024-01-05 02:06:47 +0200

    Add javadoc comments in ConfirmBox.java

--- src/main/java/graphics/ConfirmBox.java
+++ src/main/java/graphics/ConfirmBox.java
@@ -5,6 +5,12 @@ import java.util.Optional;
 
 public class ConfirmBox {
 
+    /** Method: display(String, String) creates a confirmation box. 
+     * This box appears when the user presses the X button.
+     * @param title is the title of the confirmation box.
+     * @param message is the message of the confirmation box.
+     * @return true if the user presses OK, false otherwise.
+     */
     public static boolean display(String title, String message) {
         Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
         alert.setTitle(title);
