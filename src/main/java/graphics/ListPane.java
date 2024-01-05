6d0621c87852f46b457ea63adcd112899dd04cb2
Cristian Scobioala - Nicoglu <t8220137@aueb.gr>
2024-01-06 01:40:07 +0200

    Add scrolling pane with specific info about containers

--- src/main/java/graphics/ListPane.java
+++ src/main/java/graphics/ListPane.java
@@ -31,25 +31,27 @@ public class ListPane {
      *  contains the checkboxes of the containers as well.
      * @return grid: the grid that contains the list of containers.
      */
-    public GridPane getGrid() {
+    public GridPane getListGrid() {
         
         /* Create and set the grid.*/
         grid = new GridPane();
         grid.setHgap(70);
         grid.setVgap(20);
         grid.setAlignment(javafx.geometry.Pos.TOP_CENTER);
-        grid.setPadding(new javafx.geometry.Insets(70, 100, 0, 0));
+        grid.setPadding(new javafx.geometry.Insets(50, 80, 100, 50));
         
         /* Create the title of the column, containing the names of the containers.*/
         Label name = new Label("Container Name");
         name.setStyle("-fx-font-weight: bold;");
         name.setFont(Font.font("Tahoma", FontWeight.BOLD, 15));
+        name.setPadding(new javafx.geometry.Insets(-30, 0, 0, 0));
         GridPane.setConstraints(name, 1, 0);
 
         /* Create the title of the column, containing the ids of the containers.*/
         Label id = new Label("Container ID");
         id.setStyle("-fx-font-weight: bold;");
         id.setFont(Font.font("Tahoma", FontWeight.BOLD, 15));
+        id.setPadding(new javafx.geometry.Insets(-30, 0, 0, 0));
         GridPane.setConstraints(id, 2, 0);
 
         /* Create the title of the column, containing the status of the containers
@@ -58,6 +60,7 @@ public class ListPane {
         Label status = new Label("Status");
         status.setStyle("-fx-font-weight: bold;");
         status.setFont(Font.font("Tahoma", FontWeight.BOLD, 15));
+        status.setPadding(new javafx.geometry.Insets(-30, 0, 0, 0));
         GridPane.setConstraints(status, 3, 0);
         
         /* Create the title of the column, containing the the time the containers
@@ -66,6 +69,7 @@ public class ListPane {
         Label time = new Label("Time Created");
         time.setStyle("-fx-font-weight: bold;");
         time.setFont(Font.font("Tahoma", FontWeight.BOLD, 15));
+        time.setPadding(new javafx.geometry.Insets(-30, 0, 0, 0));
         GridPane.setConstraints(time, 4, 0);
 
         MonitorAPI monitorAPI = new MonitorAPI();
@@ -127,10 +131,10 @@ public class ListPane {
     /** Method: getTitle() creates the title of the list of containers.
      * @return listOfContainers: the title of the list of containers.
      */
-    public Text getTitle() {
-        Text listOfContainers = new Text("List of Containers");
-        listOfContainers.setFont(Font.font("Tahoma", FontWeight.BOLD, 30)); // book old ..
-        listOfContainers.setStyle("-fx-fill: #2A2A72;");
+    public Text getText(String text) {
+        Text listOfContainers = new Text(text);
+        listOfContainers.setFont(Font.font("Tahoma", FontWeight.BOLD, 20)); // book old ..
+        //listOfContainers.setStyle("-fx-fill: #2A2A72;");
         return listOfContainers;
     }
 
@@ -152,17 +156,99 @@ public class ListPane {
      * @return vbox: the list of containers.
      */
     public VBox createList() {
-        GridPane grid = getGrid();
-        ScrollPane scrollPane = new ScrollPane(grid);
-        scrollPane.setFitToWidth(true);
+        GridPane grid = getListGrid();
+        grid.setStyle("-fx-background-color: #FFFFFF;");
+        ScrollPane listScrollPane = new ScrollPane(grid);
+        
+        listScrollPane.setHbarPolicy(ScrollPane.ScrollBarPolicy.ALWAYS);
+        listScrollPane.setVbarPolicy(ScrollPane.ScrollBarPolicy.ALWAYS);
+        listScrollPane.setMinHeight(500);
+        
+       /*  scrollPane.setFitToWidth(true);
         scrollPane.setFitToHeight(true);
-
-        Text title = getTitle();
-
+        */
+        Text title = getText("List of Containers");
+        Text info = getText("Information");
         VBox vbox = new VBox();
-        vbox.getChildren().addAll(title, scrollPane);
-        vbox.setAlignment(Pos.TOP_CENTER);
+        GridPane infoGrid = getInfoGrid();
+       
+        infoGrid.setStyle("-fx-background-color: #FFFFFF;");
+        ScrollPane infoScrollPane = new ScrollPane(infoGrid);
+        //scrollPane2.setPadding(new javafx.geometry.Insets(50, 0, 0, 0));
+        
+        infoScrollPane.setHbarPolicy(ScrollPane.ScrollBarPolicy.ALWAYS);
+        infoScrollPane.setVbarPolicy(ScrollPane.ScrollBarPolicy.ALWAYS);
+        infoScrollPane.setMinHeight(200);
+        infoScrollPane.setMaxWidth(400);
+
+        vbox.getChildren().add(title);
+        vbox.setSpacing(20);
+        vbox.getChildren().add(listScrollPane);
+        vbox.setSpacing(20);
+        vbox.getChildren().add(info);
+        vbox.setSpacing(10);
+        vbox.getChildren().add(infoScrollPane);
+        vbox.setAlignment(Pos.TOP_LEFT);
+        vbox.setPadding(new javafx.geometry.Insets(0, 50, 300, 50));
+        
         return vbox;
     
     }
+
+    public GridPane getInfoGrid() {
+        /* Create and set the grid.*/
+        grid = new GridPane();
+        grid.setHgap(70);
+        //grid.setVgap(10);
+        grid.setAlignment(javafx.geometry.Pos.BOTTOM_LEFT);
+       
+        /* Create the title of the column, containing the names of the containers.*/
+        Label name = new Label("Container Name:");
+        name.setFont(Font.font("Tahoma", FontWeight.BOLD, 15));
+        GridPane.setConstraints(name, 0, 0);
+        
+        Label id = new Label("Container ID:");
+        id.setFont(Font.font("Tahoma", FontWeight.BOLD, 15));
+        GridPane.setConstraints(id, 0, 1);
+        
+        Label status = new Label("Status:");
+        status.setFont(Font.font("Tahoma", FontWeight.BOLD, 15));
+        GridPane.setConstraints(status, 0, 2);
+
+        Label imageId = new Label("Image ID:");
+        imageId.setFont(Font.font("Tahoma", FontWeight.BOLD, 15));
+        GridPane.setConstraints(imageId, 0, 3);
+
+        Label networkId = new Label("Network ID:");
+        networkId.setFont(Font.font("Tahoma", FontWeight.BOLD, 15));
+        GridPane.setConstraints(networkId, 0, 4);
+
+        Label gateway = new Label("Gateway:");
+        gateway.setFont(Font.font("Tahoma", FontWeight.BOLD, 15));
+        GridPane.setConstraints(gateway, 0, 5);
+
+        Label ipAddress = new Label("IP Address:");
+        ipAddress.setFont(Font.font("Tahoma", FontWeight.BOLD, 15));
+        GridPane.setConstraints(ipAddress, 0, 6);
+
+        Label macAddress = new Label("MAC Address:");
+        macAddress.setFont(Font.font("Tahoma", FontWeight.BOLD, 15));
+        GridPane.setConstraints(macAddress, 0, 7);
+        
+        grid.getChildren().addAll(name, 
+                                  id, 
+                                  status, 
+                                  imageId, 
+                                  networkId, 
+                                  gateway, 
+                                  ipAddress, 
+                                  macAddress);
+
+        return grid;
+    }
+    
+
+
+
+
 }
