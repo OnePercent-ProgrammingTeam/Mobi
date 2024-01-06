0e836d03cf5ca0da090306942befd6169461be24
Dionisia <t8220066@aueb.gr>
2024-01-06 22:30:52 +0200

    Add a method in comments to create a list with the running containers

--- src/main/java/gr/aueb/dmst/onepercent/programming/MonitorAPI.java
+++ src/main/java/gr/aueb/dmst/onepercent/programming/MonitorAPI.java
@@ -29,6 +29,20 @@ public class MonitorAPI extends SuperAPI {
         }); // add all containers to containerModels list
     }
 
+    /* 
+    List<ContainerModel> containerRunning = new ArrayList();
+
+    public void initializeContainerRunning() {
+        List<Container> containers;
+        containers = MonitorAPI.dc.listContainersCmd().withShowAll(false).exec();
+        containers.forEach(c -> {
+            if (c != null)
+                 containerRunning.add(new ContainerModel(c));
+        }); // add only running containers to containerModels list
+    }
+    */
+
+
    /** Method: getContainerList() prints the names of the locally installed containers, their ids, 
     *  their status and the time they were created. 
     */
