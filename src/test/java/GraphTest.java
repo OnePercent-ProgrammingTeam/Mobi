7fb61b68c50b58dee89223bf9ad0fbfff24af5a5
zoi limperi <t8220079@aueb.gr>
2024-01-09 15:52:16 +0200

    Create a test class for Graph class

---
+++ src/test/java/GraphTest.java
@@ -0,0 +1,15 @@
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import org.junit.jupiter.api.Test;
+import gr.aueb.dmst.onepercent.programming.Graph;
+
+public class GraphTest {
+    @Test
+    public void getInstanceTest() {
+        Graph obj = Graph.getInstance();
+        assertNotNull(obj);
+        assertEquals(obj, Graph.getInstance());
+    }
+}
+    
+
