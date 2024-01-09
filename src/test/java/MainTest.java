0a8489f111acf0caba8f9eeb17f0f0de2d6a733c
zoi limperi <t8220079@aueb.gr>
2024-01-09 15:53:07 +0200

    Create a test class for Main class

---
+++ src/test/java/MainTest.java
@@ -0,0 +1,18 @@
+/*import static org.junit.jupiter.api.Assertions.assertEquals;
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
+import org.junit.jupiter.api.Test;
+import gr.aueb.dmst.onepercent.programming.Main;
+*/
+public class MainTest {
+   /*  @Test
+    public void handleInputTest() {
+        String s = "demo";
+        String temp = s + "\n";
+        InputStream in = new ByteArrayInputStream(temp.getBytes());
+        System.setIn(in);
+        String result = Main.handleInput("test");
+        System.setIn(System.in);
+        assertEquals(s, result);
+    }  */
+}
