ffe8809274ff58df9cd919892e6f20696fb3d930
zoi limperi <t8220079@aueb.gr>
2024-01-10 01:01:48 +0200

    Create a class in order to test MenuThreadCLI class

---
+++ src/test/java/MenuThreadCLITest.java
@@ -0,0 +1,30 @@
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
+
+import org.junit.jupiter.api.Test;
+import gr.aueb.dmst.onepercent.programming.MenuThreadCLI;
+
+public class MenuThreadCLITest {
+    MenuThreadCLI obj = new MenuThreadCLI();
+
+    @Test
+    public void testHandleUserInput() {
+        String s = "test\n";
+        InputStream in = new ByteArrayInputStream(s.getBytes());
+        System.setIn(in);
+        assertTrue(obj.handleUserInput()); //check input mismatch case
+
+        s = "1\nY\n";
+        in = new ByteArrayInputStream(s.getBytes()); 
+        //check the case that user gives number and press Y  
+        System.setIn(in);
+        assertTrue(obj.handleUserInput());
+
+        // if user presses number and N there is no return statement as the app closes 
+        //so we cannot test this case 
+    }
+    
+}
