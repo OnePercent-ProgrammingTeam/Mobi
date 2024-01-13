eb25980abdee2492cc336897ab373a720c69fba6
zoi limperi <t8220079@aueb.gr>
2024-01-13 16:51:59 +0200

    Modify ManagerApiTest class so as to work properly

--- src/test/java/ManagerAPITest.java
+++ src/test/java/ManagerAPITest.java
@@ -1,13 +1,18 @@
-/* import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import java.io.ByteArrayInputStream;
 import java.io.InputStream;
+import java.util.List;
+
 import org.junit.jupiter.api.Test;
+
+import com.github.dockerjava.api.model.Container;
+
 import gr.aueb.dmst.onepercent.programming.ManagerAPI;
 
 public class ManagerAPITest {
     ManagerAPI obj = new ManagerAPI();
+    TestsHelper obj2 = TestsHelper.getInstance();
     @Test
     void handleInputTest() {
         obj.createDockerClient();
@@ -22,17 +27,23 @@ public class ManagerAPITest {
     @Test
     void checkActiveContainerStatus() {
         obj.createDockerClient();
-        String id = "91ae36fe661b1b81b588a68efee2acb162584e46dd5cf91080abddd581a9bfac";
+        String id = obj2.getTestid();
         boolean result = obj.checkActiveContainerStatus(id);
-        assertFalse(result);
+        assertEquals(result, obj2.checkContainerStatus(id));
     }
     @Test
     void checkAllContainerStatus() {
         obj.createDockerClient();
-        String id = "91ae36fe661b1b81b588a68efee2acb162584e46dd5cf91080abddd581a9bfac";
+        String id = obj2.getTestid();
         boolean result = obj.checkAllContainerStatus(id);
-        assertTrue(result);
+        List<Container> arr = obj2.getAllContainers();
+        boolean flug = false;
+        for (Container c : arr) {
+            if (c.getId().equals(id))
+                flug = true;
+        }
+        assertTrue(flug);
     }
 }
 
-*/
+
