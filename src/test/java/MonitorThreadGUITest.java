91221da9d0b909ba854a35acdedf708b41f2c078
zoi limperi <t8220079@aueb.gr>
2024-01-13 16:41:10 +0200

    Create a test class in order to test MonitorThreadGUI class

---
+++ src/test/java/MonitorThreadGUITest.java
@@ -0,0 +1,18 @@
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+import org.junit.jupiter.api.Test;
+
+import gr.aueb.dmst.onepercent.programming.MonitorThreadGUI;
+
+public class MonitorThreadGUITest {
+    @Test
+      public void testGetInstance() {
+        MonitorThreadGUI obj = MonitorThreadGUI.getInstance();
+        assertNotNull(obj);
+        MonitorThreadGUI obj2 = MonitorThreadGUI.getInstance();
+        assertEquals(obj, obj2);
+
+    }
+    
+}
