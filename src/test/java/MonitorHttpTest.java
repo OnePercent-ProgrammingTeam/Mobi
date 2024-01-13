71e8ddee46088976bb2a6efcc329169137e223ec
zoi limperi <t8220079@aueb.gr>
2024-01-13 16:58:11 +0200

    Modify MonitorHttpTest class so as to work properly

--- src/test/java/MonitorHttpTest.java
+++ src/test/java/MonitorHttpTest.java
@@ -1,20 +1,23 @@
-//import com.fasterxml.jackson.core.JsonProcessingException;
-//import gr.aueb.dmst.onepercent.programming.MonitorHttp;
-//import org.junit.jupiter.api.BeforeEach;
-//import org.junit.jupiter.api.Test;
-//import static org.junit.jupiter.api.Assertions.fail;
-//import static org.junit.jupiter.api.Assertions.assertEquals;
-//import static org.junit.jupiter.api.Assertions.assertNotNull;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import gr.aueb.dmst.onepercent.programming.MonitorHttp;
+import gr.aueb.dmst.onepercent.programming.MonitorHttpCLI;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.fail;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 
 public class MonitorHttpTest {
 
-   /*  private MonitorHttp containerMonitor;
+    TestsHelper obj = TestsHelper.getInstance();
+    private MonitorHttpCLI containerMonitor;
 
     @BeforeEach
     // Create a new instance of MonitorHttp before each test
     public void setUp() {
-        containerMonitor = new MonitorHttp();
+        containerMonitor = new MonitorHttpCLI();
     }
 
     @Test
@@ -35,33 +38,21 @@ public class MonitorHttpTest {
     }
 
     
-   /*  @Test
+    @Test
        public void testPrepareStorageData() {
     // Test the testPrepareStorageData method and assert the result
-        MonitorHttp.containerId = 
-            "eae733773feefb6adbbb2226c20eb9125adbe4d1fb69da2b1cafd6304eb11c8c";
-        MonitorHttp.response1 = 
-                new StringBuffer("{ \"Name\": \"/optimistic_cerf\"," + 
-                "\"Id\": \"eae733773feefb6adbbb2226c20eb9125adbe4d1fb69da2b1cafd6304eb11c8c\", " +
-                "\"NetworkSettings\": { \"Networks\": { \"bridge\": " +
-                "{ \"IPAddress\": \"172.17.0.2\", \"MacAddress\": \"02:42:ac:11:00:02\" } } }," +
-                 "\"cpu_stats\": { \"cpu_usage\": { \"total_usage\": 100 }, " +
-                 "\"system_cpu_usage\": 200, \"online_cpus\": 4 }, \"precpu_stats\":" +
-                  "{ \"cpu_usage\": { \"total_usage\": 50 }, \"system_cpu_usage\": 100 } } }");
-
+        MonitorHttp.containerId = obj.getTestid();
+        
         try {
             String[] result = containerMonitor.prepareStoragedData();
             assertNotNull(result);
             assertEquals(6, result.length);
-            assertEquals("optimistic_cerf", result[0]);
-            assertEquals("eae733773feefb6adbbb2226c20eb9125adbe4d1fb69da2b1cafd6304eb11c8c", 
+            assertEquals(obj.getTesContainer().getNames()[0].substring(1), result[0]);
+            assertEquals(obj.getTesContainer().getId(), 
                 result[1]);
-            assertEquals("172.17.0.2", result[2]);
-            assertEquals("02:42:ac:11:00:02", result[3]);
-            //assertEquals("200.0", result[4]); 
-            //we can not check the real time date and time 
         } catch (Exception e) {
             fail("Exception not expected", e);
         }
-    }*/
+    }
 }
+
