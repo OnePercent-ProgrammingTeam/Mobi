9ed223866be52ac790fa2b66e04e7adcea35dafc
zoi limperi <t8220079@aueb.gr>
2024-01-13 16:54:35 +0200

    Modify MenuThreadCLITest class so as to work properly

--- src/test/java/MenuThreadCLITest.java
+++ src/test/java/MenuThreadCLITest.java
@@ -2,12 +2,14 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.io.ByteArrayInputStream;
 import java.io.InputStream;
+import java.util.NoSuchElementException;
 
 import org.junit.jupiter.api.Test;
 import gr.aueb.dmst.onepercent.programming.MenuThreadCLI;
 
 public class MenuThreadCLITest {
     MenuThreadCLI obj = new MenuThreadCLI();
+
 
     @Test
     public void testHandleUserInput() {
@@ -16,14 +18,20 @@ public class MenuThreadCLITest {
         System.setIn(in);
         assertTrue(obj.handleUserInput()); //check input mismatch case
 
-        s = "1\nY\n";
+        s = "8\nY\n";
         in = new ByteArrayInputStream(s.getBytes()); 
         //check the case that user gives number and press Y  
         System.setIn(in);
-        assertTrue(obj.handleUserInput());
+        try {
+            assertTrue(obj.handleUserInput());
+        } catch (NoSuchElementException e) {
+            System.out.println("Executor Thread Exception");
+        }
+
+    }
+}
+
 
         // if user presses number and N there is no return statement as the app closes 
         //so we cannot test this case 
-    }
-    
-}
+
