00b7ac059611d3dd62924f45f61dace7b57f1e16
Andreas <t8220168@aueb.gr>
2024-01-14 20:06:48 +0200

    Alter the JavaDoc comments in class MenuThread where needed

--- src/main/java/gr/aueb/dmst/onepercent/programming/MenuThread.java
+++ src/main/java/gr/aueb/dmst/onepercent/programming/MenuThread.java
@@ -9,12 +9,26 @@ public class MenuThread extends Thread {
     @Override
     public void run() { }
 
+    /**
+     * Waits for the thread to complete its execution. This method blocks the calling thread
+     * until the thread on which it is called terminates or is interrupted.
+     *
+     * throws InterruptedException If the current thread is interrupted 
+     * while waiting for the associated thread to complete.
+     */
     public void waitThread() {
         try {
             thread.join();
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
+
+    }
+
+    /** 
+     * Default Constructor
+     */
+    public MenuThread() {
 
     }
 }
