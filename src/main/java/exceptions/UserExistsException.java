026613085cb7627aa9b9836d0ea54e0be023b025
Dionisia <t8220066@aueb.gr>
2024-01-16 17:04:18 +0200

    Add javadoc comments

--- src/main/java/exceptions/UserExistsException.java
+++ src/main/java/exceptions/UserExistsException.java
@@ -1,8 +1,18 @@
 package exceptions;
-
+/** Class: UserExistsException is the class that creates an exception 
+ * if the user has already signed up */
 public class UserExistsException extends Exception {
+
+    /** 
+     * Default Constructor
+     */
     public UserExistsException() { }
 
+
+    /** 
+    * Constructor that show a message
+    * @param username The name of the user
+    */
     public UserExistsException(String username) {
         super("User with the name " + username + " and the specific password already exists");
     }
--- src/main/java/exceptions/UserNotFoundException.java
+++ src/main/java/exceptions/UserNotFoundException.java
@@ -1,8 +1,17 @@
 package exceptions;
 
+/** Class: UserNotFoundException is the class that creates an exception 
+ * if the user does not has an acoount */
 public class UserNotFoundException extends Exception {
+    /** 
+     * Default Constructor
+     */
     public UserNotFoundException() { }
 
+    /** 
+    * Constructor that show a message
+    * @param username The name of the user
+    */
     public UserNotFoundException(String username) {
         super("User with the name '" + username + "'' and the specific password does not exists");
     }
