624f71898ac2ff35e3a96be369c6a7373bd7d738
Cristian Scobioala - Nicoglu <t8220137@aueb.gr>
2024-01-17 18:39:57 +0200

    Create Main page for gui

--- src/main/java/gr/aueb/dmst/onepercent/programming/graphics/MainPageController.java
+++ src/main/java/gr/aueb/dmst/onepercent/programming/graphics/MainPageController.java
@@ -1,9 +1,50 @@
 package gr.aueb.dmst.onepercent.programming.graphics;
+
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.control.Button;
+import javafx.scene.layout.AnchorPane;
 
 public class MainPageController {
-    
+
     @FXML
     private Button containersButton;
+
+    @FXML
+    public AnchorPane contentArea;
+
+    @FXML
+    private Button imagesButton;
+
+    @FXML
+    private void initialize() {
+        System.out.println("here");
+        loadPage("ContainersPage.fxml");
+    }
+
+    @FXML
+    void loadContainers(ActionEvent event) {
+        loadPage("ContainersPage.fxml");
+    }
+
+    @FXML
+    void loadImages(ActionEvent event) {
+        loadPage("ImagesPage.fxml");
+    }
+
+    private void loadPage(String pageName) {
+        try {
+            // Load the FXML file for the selected page
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/" + pageName));
+            Parent pageRoot = loader.load();
+            // Set the content area with the loaded page
+            contentArea.getChildren().setAll(pageRoot);
+        } catch (Exception e) {
+            e.printStackTrace();
+            // Handle the exception as needed
+        }
+    }
+
 }
