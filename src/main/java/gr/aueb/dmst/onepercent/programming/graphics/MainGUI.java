e8d48ad64e203fb2a803c2e738f12a5e9c193fdd
chrissel04 <t8220135@aueb.gr>
2024-01-17 15:50:58 +0200

    Create new Main class for the GUI

--- src/main/java/gr/aueb/dmst/onepercent/programming/graphics/MainGUI.java
+++ src/main/java/gr/aueb/dmst/onepercent/programming/graphics/MainGUI.java
@@ -4,6 +4,7 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
 public class MainGUI extends Application {
     public static void main(String[] args) {
@@ -11,9 +12,11 @@ public class MainGUI extends Application {
     }
 
     public void start(Stage primaryStage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("/fxml/MainPage.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("/fxml/LoginPage.fxml"));
         primaryStage.setTitle("One Percent");
-        primaryStage.setScene(new Scene(root, 600, 400));
+        primaryStage.setScene(new Scene(root, 1000, 600));
+        primaryStage.setResizable(false); //does not allow the user to resize the window
+        primaryStage.initStyle(StageStyle.DECORATED.UNDECORATED);
         primaryStage.show();
     }
 }
