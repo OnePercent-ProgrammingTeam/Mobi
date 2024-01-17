de85d35e264c42bd2d2ee6893960e44d237efde0
Dionisia <t8220066@aueb.gr>
2024-01-17 02:43:30 +0200

    Add the target files

--- target/apidocs/package-search-index.js
+++ target/apidocs/package-search-index.js
@@ -1,1 +1,1 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"exceptions"},{"l":"gr.aueb.dmst.onepercent.programming"},{"l":"graphics"}];updateSearchResults();
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"exceptions"},{"l":"gr.aueb.dmst.onepercent.programming.cli"},{"l":"gr.aueb.dmst.onepercent.programming.core"},{"l":"gr.aueb.dmst.onepercent.programming.gui"},{"l":"graphics"}];updateSearchResults();
