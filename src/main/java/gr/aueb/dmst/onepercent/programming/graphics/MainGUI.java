7f8561914d4adb53eec5140cb45d5c717fbc86f6
Cristian Scobioala - Nicoglu <t8220137@aueb.gr>
2024-01-17 18:40:18 +0200

    Create Main GUI, core of GUI

--- src/main/java/gr/aueb/dmst/onepercent/programming/graphics/MainGUI.java
+++ src/main/java/gr/aueb/dmst/onepercent/programming/graphics/MainGUI.java
@@ -7,16 +7,19 @@ import javafx.stage.Stage;
 import javafx.stage.StageStyle;
 
 public class MainGUI extends Application {
+    static Stage window;
+
     public static void main(String[] args) {
         launch(args);
     }
 
     public void start(Stage primaryStage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("/fxml/LoginPage.fxml"));
-        primaryStage.setTitle("One Percent");
-        primaryStage.setScene(new Scene(root, 1000, 600));
-        primaryStage.setResizable(false); //does not allow the user to resize the window
-        primaryStage.initStyle(StageStyle.DECORATED.UNDECORATED);
-        primaryStage.show();
+        Parent root = FXMLLoader.load(getClass().getResource("/fxml/MainPage.fxml"));
+        window = primaryStage;
+        window.setTitle("One Percent");
+        window.setScene(new Scene(root, 1000, 600));
+        window.setResizable(false); //does not allow the user to resize the window
+        window.initStyle(StageStyle.DECORATED.UNDECORATED);
+        window.show();
     }
 }
